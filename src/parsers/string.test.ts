25bae83893d47986ef8355f06c1c5003fd051d94
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-01-25 00:41:06 +0100

    Update build and lint configurations (#79)

--- src/parsers/string.test.ts
+++ src/parsers/string.test.ts
@@ -10,16 +10,3 @@ describe('encodeString', () => {
     );
   });
 });
-
-// TODO
-/*describe('decodeString', () => {
-  it('decodes a byte array to a buffer', () => {
-    const buffer = Buffer.from(
-      '00000000000000000000000000000000000000000000000000000000000000374c6f72656d20697073756d20646f6c6f722073697420616d65742c20636f6e73656374657475722061646970697363696e6720656c6974000000000000000000',
-      'hex'
-    );
-    expect(decodeString(buffer, new Uint8Array(0), 'bytes')).toBe(
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-    );
-  });
-});*/
