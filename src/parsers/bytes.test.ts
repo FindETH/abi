ee2d5263f438f79ab286745de72d4decd523a705
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-03-07 14:31:14 +0100

    Fix bytes parser (#114)
    * Fix bytes parser
    
    * Remove TODO comment

--- src/parsers/bytes.test.ts
+++ src/parsers/bytes.test.ts
@@ -1,5 +1,5 @@
-import { fromUtf8, toHex } from '../utils';
-import { encodeBytes } from './bytes';
+import { fromHex, fromUtf8, toHex, toUtf8 } from '../utils';
+import { decodeBytes, encodeBytes } from './bytes';
 
 describe('encodeBytes', () => {
   it('encodes a byte string to a buffer', () => {
@@ -7,5 +7,14 @@ describe('encodeBytes', () => {
     expect(toHex(encodeBytes(new Uint8Array(0), bytes, 'bytes'))).toBe(
       '00000000000000000000000000000000000000000000000000000000000000374c6f72656d20697073756d20646f6c6f722073697420616d65742c20636f6e73656374657475722061646970697363696e6720656c6974000000000000000000'
     );
+  });
+});
+
+describe('decodeBytes', () => {
+  it('decodes a string from a buffer', () => {
+    const value = fromHex(
+      '000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000374c6f72656d20697073756d20646f6c6f722073697420616d65742c20636f6e73656374657475722061646970697363696e6720656c6974000000000000000000'
+    );
+    expect(toUtf8(decodeBytes(value, value, 'bytes'))).toBe('Lorem ipsum dolor sit amet, consectetur adipiscing elit');
   });
 });
--- src/parsers/bytes.ts
+++ src/parsers/bytes.ts
@@ -11,7 +11,7 @@ export const encodeBytes: EncodeFunction = (buffer: Uint8Array, value: string | 
 // TODO: This may not work properly yet
 export const decodeBytes: DecodeFunction = (value: Uint8Array, buffer: Uint8Array): Uint8Array => {
   const pointer = Number(toNumber(value.subarray(0, 32)));
-  const length = toNumber(value.subarray(pointer, pointer + 32));
+  const length = Number(toNumber(buffer.subarray(pointer, pointer + 32)));
 
-  return buffer.subarray(32, 32 + Number(length));
+  return buffer.subarray(pointer + 32, pointer + 32 + Number(length));
 };
